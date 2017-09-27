5739772874e638ce02c81b928698748eb922d196
takeuchihidekijp <takeuchihidekijp@yahoo.co.jp>
2017-09-28 03:33:48 +0900

    Initial Commit

---
+++ proto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  proto
+//
+//  Created by 竹内秀樹 on 2017/09/28.
+//  Copyright © 2017年 hideki.takeuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
