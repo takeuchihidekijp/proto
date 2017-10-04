506051d0be2315fc7e6361b6e40f807fb85f1270
takeuchihidekijp <takeuchihidekijp@yahoo.co.jp>
2017-10-04 19:37:02 +0900

    メンタリング前

--- Podfile
+++ Podfile
@@ -4,4 +4,5 @@ use_frameworks!
 target 'proto' do
   pod 'Alamofire', '~> 4.5'
   pod 'SwiftyJSON', '4.0.0-alpha.1'
+  pod 'GoogleMaps', '~> 2.4'
 end
--- proto/ViewController.swift
+++ proto/ViewController.swift
@@ -7,12 +7,41 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
+    
+    var googleMap : GMSMapView!
+    
+    //緯度経度 -> 金沢駅
+    let latitude: CLLocationDegrees = 36.5780574
+    let longitude: CLLocationDegrees = 136.6486596
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ズームレベル.
+        let zoom: Float = 15
+        
+        // カメラを生成.
+        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: latitude,longitude: longitude, zoom: zoom)
+        
+        // MapViewを生成.
+        googleMap = GMSMapView(frame: CGRect(x:0, y:0, width:self.view.bounds.width, height:self.view.bounds.height))
+        
+        // MapViewにカメラを追加.
+        googleMap.camera = camera
+        
+        //マーカーの作成
+        let marker: GMSMarker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(latitude, longitude)
+        marker.map = googleMap
+        
+        
+        //viewにMapViewを追加.
+        self.view.addSubview(googleMap)
+        
     }
 
     override func didReceiveMemoryWarning() {
